749f71faa57cb0f46debd13dfa8d5eb88da0b9ee
ridaabilals <ridaabilal49@gmail.com>
2025-12-01 19:36:50 +0500

    Added Request class implementation

--- src/Request.cpp
+++ src/Request.cpp
@@ -12,4 +12,5 @@ void Request::displayRequest() const {
          << "Book: " << bookId 
          << " | User: " << userId 
          << " | Status: " << status << "\n";
+    cout<<"/n-------------------------\n";     
 }
