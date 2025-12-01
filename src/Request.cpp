542ea7b376d616bc06cc47bf125a62eda41cd984
ridaabilals <ridaabilal49@gmail.com>
2025-12-01 19:34:24 +0500

    Added Request class implementation

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,5 +1,15 @@
 #include "Request.h"
+#include <iostream>
+using namespace std;
 
-Request::Request() {
-    // Will be implemented later
+Request::Request() {}
+
+Request::Request(int id, int bookId, int userId)
+    : id(id), bookId(bookId), userId(userId), status("PENDING") {}
+
+void Request::displayRequest() const {
+    cout << "\n[Request ID: " << id << "] "
+         << "Book: " << bookId 
+         << " | User: " << userId 
+         << " | Status: " << status << "\n";
 }
