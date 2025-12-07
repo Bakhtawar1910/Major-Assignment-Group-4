e08cf00be3001166e7e1847c0095a907c7180807
Bakhtawar1910 <Biakhan2005@gmail.com>
2025-12-07 20:25:51 +0500

    Corrected book.cpp code

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,10 +1,14 @@
-void ExchangeSystem::saveBooks() {
-    ofstream file("data/books.txt");
-    for (auto &b : books) {
-        file << b.getId() << "|"
-             << b.getTitle() << "|"
-             << b.getAuthor() << "|"
-             << b.getGenre() << "\n";
-    }
-    file.close();
+#include "Book.h"
+#include <iostream>
+using namespace std;
+
+Book::Book() {}
+
+Book::Book(int id, string title, string author, string genre)
+    : id(id), title(title), author(author), genre(genre) {}
+
+void Book::displayBook() const {
+    cout << "\n[Book ID: " << id << "] "
+         << title << " by " << author 
+         << " (" << genre << ")\n";
 }
