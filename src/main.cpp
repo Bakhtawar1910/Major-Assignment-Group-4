99b82ccb2aa6d912fdcd05fc076f7820efc8e330
Bakhtawar1910 <Biakhan2005@gmail.com>
2025-12-07 18:59:49 +0500

    Added main function to the code

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "ExchangeSystem.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    ExchangeSystem system;  // initialize system (loads data)
+    system.run();           // start menu loop
+    return 0;
+}
